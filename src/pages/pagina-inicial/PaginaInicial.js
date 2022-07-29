f5d737299b0c734fa860765441d8ee0fc1a39671
GiovanneAndrade <vanne.as@hotmail.com>
2022-07-28 21:33:54 -0300

    atualizando

--- src/pages/pagina-inicial/PaginaInicial.js
+++ src/pages/pagina-inicial/PaginaInicial.js
@@ -1,47 +1,48 @@
 import "./paginaInicial.css";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Top from '../../components/top/Top'
 import img from "./image/image 3.png"
+import Horario from "../horario/Horario"
 
 
-export default function PaginaInicial (){
-  const filmes = [{
-    id: 1,
-    img: "",
-    nome:""
-  },
-]
+export default function PaginaInicial (props){
+
+  const [filmes, setFilmes] = useState([])
+
+  useEffect(() => {
+    fetch('https://mock-api.driven.com.br/api/v7/cineflex/movies')
+    .then(res => res.json())
+    .then(data => setFilmes(data))
+  },[])
+
   return (
   <>
-  
   <Top texto={"Selecione o filme"}/>
-  <div className="pagina-inicial-filmes">
- <div className="lista-filmes">
- <Link to="/Horario" >
-  <div className="filmes">
-      <img src={img}/>
-    </div>
-  </Link> 
-  <Link to="/Horario" >
-  <div className="filmes">
-      <img src={img}/>
-    </div>
-  </Link> 
-  <Link to="/Horario" >
-  <div className="filmes">
-      <img src={img}/>
-    </div>
-  </Link> 
-  <Link to="/Horario" >
-  <div className="filmes">
-      <img src={img}/>
-    </div>
-  </Link> 
-    
-    
-  </div>
+  {filmes.map((item, index) =>(
+  <ArrayFilmes key={index}   imagem={item.posterURL} nome={item.nome}/>)
+)}
   
-  </div>
   </>)
   
+}
+
+function ArrayFilmes(
+  {
+  imagem
+  }
+  ) {
+  return(
+    <>
+      <div className="pagina-inicial-filmes">
+        <div className="lista-filmes">
+          <Link to="/Horario" >
+            <div className="filmes">
+               <img src={imagem} /> 
+            </div>
+          </Link> 
+        </div>
+      </div>
+    </>
+  )
 }
